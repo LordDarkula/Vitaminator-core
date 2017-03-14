f9b5fecdda688fa12fdffd5926c1e710506133a6
Aubhro <aubhrosengupta@gmail.com>
2017-03-13 18:12:00 -0700

    Edit main

--- tensorflow_model.py
+++ tensorflow_model.py
@@ -85,34 +85,16 @@ def run_tensorflow_model():
         for epoch in range(500):
             epoch_loss = 0
             avg_cost = 0.
-            i = 0
             for i in range(n_batches):
                 batch_x, batch_y = next_batch(batch_size, i, train_X), next_batch(batch_size, i, train_y)
 
-                # batch = (train_X[batch_size * i:batch_size * (i + 1)], train_y[batch_size * i:batch_size * (i + 1)])
                 _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.5})
                 epoch_loss += c
                 avg_cost += c / n_batches
-                # i += batch_size
-                # print("the loss of " + str(i) + "out of " + str(n_batches) + " is " + str(c))
 
             print('Epoch', epoch + 1, 'completed out of', 500, 'loss:', epoch_loss, "cost:", avg_cost)
         print('Accuracy:', accuracy.eval({x: test_X, y_: test_y, keep_prob: 0.5}))
         saver.save(sess, 'model/my-model')
-
-        # print("On iteration number {}".format(i))
-        # train_accuracy = accuracy.eval(
-        #     feed_dict={
-        #         x: batch[0],
-        #         y_: batch[1],
-        #         keep_prob: 0.5}
-        # )
-        #
-        # print("step " + str(i) + ", training accuracy " + str(train_accuracy))
-        # optimizer.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-        #
-        # print("test accuracy %g" % accuracy.eval(feed_dict={
-        #     x: test_X, y_: test_y, keep_prob: 1.0}))
 
 
 def restore_model():
@@ -123,9 +105,9 @@ def restore_model():
 
 
 if __name__ == '__main__':
-    # If
+    # First run
     randomly_assign_train_test('images')
     run_tensorflow_model()
 
-    # run_tensorflow_model()
+    # To restore model
     restore_model()
