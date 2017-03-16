a83aeaabb290f1a0e842c4fa28503a244dd482dd
Aubhro <aubhrosengupta@gmail.com>
2017-03-15 17:29:06 -0700

    Rewrite layers and add tensorboard histograms

--- tensorflow_model.py
+++ tensorflow_model.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
-import numpy as np
 from process_images import randomly_assign_train_test
 from bottleneck_keras import save_images_to_arrays
-from tqdm import tqdm
+
 
 x = tf.placeholder(tf.float32, shape=[None, 130 * 130])
 y_ = tf.placeholder(tf.float32, shape=[None, 2])
@@ -18,16 +17,21 @@ def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial, name='B')
 
-
-def conv2d(X, W, name='conv'):
+def conv_layer(X, W, b, name='conv'):
     with tf.name_scope(name):
-        return tf.nn.conv2d(X, W, strides=[1, 1, 1, 1], padding='SAME')
+        convolution = tf.nn.conv2d(X, W, strides=[1, 1, 1, 1], padding='SAME')
+        activation = tf.nn.relu(convolution + b)
 
+        tf.summary.histogram('weights', W)
+        tf.summary.histogram('biases', b)
+        tf.summary.histogram('activation', activation)
 
-def max_pool_2x2(X, name='conv'):
+        return tf.nn.max_pool(activation, ksize=[1, 2, 2, 1], strides=[1, 1, 1, 1], padding='SAME')
+
+def fc_layer(X, n_features, W, b, name='fc'):
     with tf.name_scope(name):
-        return tf.nn.max_pool(X, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
+        X_flat = tf.reshape(X, [-1, n_features])
+        return tf.nn.relu(tf.matmul(X_flat, W) + b)
 
 
 def build_model(image_size):
@@ -36,27 +40,27 @@ def build_model(image_size):
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
 
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
+    model = conv_layer(x_image, W_conv1, b_conv1)
 
     W_conv2 = weight_variable([5, 5, 32, 64])
     b_conv2 = bias_variable([64])
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
+    print(model.shape)
 
-    W_fc1 = weight_variable([33 * 33 * 64, 1024])
+    model = conv_layer(model, W_conv2, b_conv2)
+
+    print(model.shape)
+
+    W_fc1 = weight_variable([130 * 130 * 64, 1024])
     b_fc1 = bias_variable([1024])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 33 * 33 * 64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    model = fc_layer(model, (130 * 130 * 64), W_fc1, b_fc1)
+    model = tf.nn.dropout(model, keep_prob)
 
     W_fc2 = weight_variable([1024, 2])
     b_fc2 = bias_variable([2])
 
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    y_conv = fc_layer(model, 1024, W_fc2, b_fc2)
 
     with tf.name_scope('cross_entropy'):
         cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
@@ -90,7 +94,7 @@ def run_tensorflow_model():
         print("Session starting")
 
         merged_summary = tf.summary.merge_all()
-        writer = tf.summary.FileWriter('/tmp/vitaminator/2')
+        writer = tf.summary.FileWriter('/tmp/vitaminator/6')
         writer.add_graph(sess.graph)
 
 
@@ -98,17 +102,20 @@ def run_tensorflow_model():
 
         for epoch in range(500):
             epoch_loss = 0
-            avg_cost = 0.
+            avg_cost = 0.0
             for i in range(n_batches):
                 batch_x, batch_y = next_batch(batch_size, i, train_X), next_batch(batch_size, i, train_y)
 
-                _, c, s = sess.run([optimizer, cost, merged_summary], feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.5})
+                _, cross_entropy = sess.run([optimizer, cost], feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.5})
 
-                writer.add_summary(s, i)
-                epoch_loss += c
-                avg_cost += c / n_batches
+                epoch_loss += cross_entropy
+                avg_cost += cross_entropy / n_batches
 
-            print('Epoch', epoch + 1, 'completed out of', 500, 'loss:', epoch_loss, "cost:", avg_cost)
+                if i % 5 == 0:
+                    s = sess.run(merged_summary, feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.5})
+                    writer.add_summary(s, i)
+
+            print('Epoch {} completed out of {}, loss: {}, cost: {}'.format(epoch + 1, 500, epoch_loss, avg_cost))
 
         print('Accuracy:', accuracy.eval({x: test_X, y_: test_y, keep_prob: 0.5}))
         saver.save(sess, 'model/my-model')
